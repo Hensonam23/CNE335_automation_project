64de5e3e4ddb05be5a81ea851ce2e0accfa96cda
Aaron Henson <182049784+Hensonam23@users.noreply.github.com>
2025-03-14 21:23:42 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# CNE335 Final Project
+# Aaron Henson
+# CNE 335 Winter 2025
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Aaron Henson")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
     # TODO - Call Ping method and print the results
+     server = Server()
+     server.ping()
